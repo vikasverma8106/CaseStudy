f31b4ab3fb1da7c55a2a4e25e7bc4140c83d18ad
Bhoomika <bhoomikasp231@gmail.com>
2023-09-14 17:31:09 +0530

    booking update

--- src/routes/booking/booking.jsx
+++ src/routes/booking/booking.jsx
@@ -1,50 +1,95 @@
-// BookingPage.js
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 import axios from 'axios';
 
 const BookingPage = () => {
   const { stationId } = useParams();
-  const [station, setStation] = useState(null);
+  
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
   const [availablePorts, setAvailablePorts] = useState([]);
+  const [isAvailabilityChecked, setIsAvailabilityChecked] = useState(false);
 
-  useEffect(() => {
-  // Fetch station details based on the stationId
-   axios.get(`/api/stations/${stationId}`)
+  const checkAvailability = () => {
+    const selectedStartTime = startTime;
+    const selectedEndTime = endTime;
+
+    // Define the API endpoint where you want to check availability
+    const apiUrl = '/api/check-availability'; // Replace with your actual API endpoint
+
+    // Prepare the request body with the selected time range
+    const requestBody = {
+      startTime: selectedStartTime,
+      endTime: selectedEndTime,
+    };
+
+    // Make an Axios POST request to check availability
+    axios
+      .post(apiUrl, requestBody)
       .then((response) => {
-        setStation(response.data);
-        setAvailablePorts(response.data.ports); // Assuming ports is an array in station data
+        // Handle the response from the server
+        const { availablePorts } = response.data;
+
+        // Update the availablePorts state with the received data
+        setAvailablePorts(availablePorts);
+
+        // Set the flag to indicate that availability has been checked
+        setIsAvailabilityChecked(true);
       })
       .catch((error) => {
-        console.error('Error fetching station data:', error);
+        console.error('Error checking availability:', error);
       });
-  }, [stationId]);
+  };
 
-  const handlePortBooking = (portId) => {
-    // Implement port booking logic here
-    // You can update the booking status for the selected port
-    // You may want to send a request to your server to update the booking status
+  const DisplayStatus = () => {
+    // Implement your logic here if needed
   };
 
   return (
     <div>
       <h1>Booking Page</h1>
-      {station && (
-        <div>
-          <h2>Station: {station.name}</h2>
-          <p>Location: {station.location}</p>
-          <h3>Available Ports:</h3>
-          <ul>
-            {availablePorts.map((port) => (
-              <li key={port.id}>
-                Port {port.number} - {port.status === 'available' ? 'Available' : 'Booked'}
-                <button onClick={() => handlePortBooking(port.id)}>Book</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      
+      <div>
+        <Form>
+          <Form.Group>
+            <Form.Label>Select Start Time:</Form.Label>
+            <Form.Control
+              type="datetime-local"
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Select End Time:</Form.Label>
+            <Form.Control
+              type="datetime-local"
+              value={endTime}
+              onChange={(e) => setEndTime(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" onClick={checkAvailability}>
+            Check Availability
+          </Button>
+        </Form>
+        {isAvailabilityChecked && (
+          <div>
+            <h3>Available Ports within Selected Time:</h3>
+            <ListGroup>
+              {Array.from({ length: availablePorts }).map((_, index) => (
+                <div key={index}>
+                  <ListGroup.Item>
+                    <p>Port {index + 1}</p>
+                    <Button onClick={DisplayStatus}>Book Now</Button>
+                  </ListGroup.Item>
+                </div>
+              ))}
+            </ListGroup>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
