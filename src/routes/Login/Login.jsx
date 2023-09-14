538e17c05f40d543593225560db09b2c70c520b8
pranathitelugunti <pranathi2804@gmail.com>
2023-09-14 06:49:45 +0000

    login page

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
 
 export default function Login() {
+    let [authMode, setAuthMode] = useState("signin");
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
+
+  if (authMode === "signin") {
     return (
-        <>
-        {/* <div>Login</div> */}
-        </>
-    )
+      <>
+        <div className="Auth-form-container">
+          <form className="Auth-form">
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title">Sign In</h3>
+              <div className="text-center">
+                Not registered yet?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Sign Up
+                </span>
+              </div>
+              <div className="form-group mt-3">
+                <label>Username</label>
+                <input
+                  type="text"
+                  className="form-control mt-1"
+                  placeholder="Username"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Enter Email</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="enter email"
+            />
+          </div>
+
+          <div className="form-group mt-3">
+            <label>Enter Password</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="password"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label for="role-type">Role</label>
+
+            <select name="role" id="role" className="form-control mt-1">
+              <option value="" hidden>
+                --Please choose an option--
+              </option>
+              <option value="customer">Customer</option>
+              <option value="admin">admin</option>
+            </select>
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
